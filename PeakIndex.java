45396a9e2aefdef3f59a875d46bc25556d7e9ded
vigneshvickkyy <vigneshv9008@gmail.com>
2025-02-26 14:49:21 +0530

    added PeakIndex

---
+++ PeakIndex.java
@@ -0,0 +1,21 @@
+package vignesh;
+
+public class PeakIndex {
+    public static void main(String[] args) {
+   int [] arr = {0,1,2,3,4,5,2,1};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+        static int peakIndexInMountainArray(int[] arr) {
+            int start = 0;
+            int end = arr.length-1;
+            while(start < end){
+                int mid = start + (end - start) / 2;
+                if(arr[mid]>arr[mid+1]){
+                    end = mid;
+                } else  {
+                    start = mid+1;
+                }
+            }
+            return start;
+        }
+}
